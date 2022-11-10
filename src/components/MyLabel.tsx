7066ae882bcc63c8ab4db91f70e34546946c520e
Joshua Cid <joshua.cid@virginiogomez.cl>
2022-11-10 12:50:28 -0300

    despligue primer componente

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import './myLabel.css'
+
+export interface MyLabelProps {
+    /** 
+    * Este es el mensaje a mostrar en la etiqueta 
+    */
+    label: string;
+    /** 
+    * Este es el tamaño de la etiqueta 
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /** 
+    * Si quiere todo capitalizado 
+    */
+    allCaps: boolean;
+    /*** 
+    * Colores básicos del botón
+    */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /*** 
+    * Color personalizado de la fuente
+    */
+    fontColor?: string;
+}
+
+export const MyLabel = ({
+    allCaps = false,
+    color = 'primary',
+    label = 'No label',
+    size = "normal",
+    fontColor
+}: MyLabelProps) => {
+    return (
+        <span className={`${size} text-${color} `}
+            style={{ color: fontColor }}
+        >
+            {allCaps ? label.toUpperCase() : label}
+        </span>
+    )
+}
+
